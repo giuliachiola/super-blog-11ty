10321283fe95e60d47f3c9096e7b8114e897c040
Giulia Chiola <giulia.chiola@modo.md>
2020-03-15 18:54:04 +0100

    feat: update

--- .eleventy.js
+++ .eleventy.js
@@ -11,14 +11,13 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.setDataDeepMerge(true); // used to merge 'blog.11tydata.js' tags with .md tags
 
   // eleventyConfig.addCollection("tagList", require("./_11ty/getTagList"));
-  eleventyConfig.addCollection('tagList', function(collection) {
-    let tags = []
-    const tagsToRemove = ['blog', 'projects']
-    collection.getAll().forEach(function(item) {
-      if("tags"in item.data ) {
+  eleventyConfig.addCollection("tagList", function(collection) {
+    // const valuesToRemove = ['blog', 'projects']
+    collection.getAll().forEach(function(item) { // FIXME: ciclare meglio questa porcata!
+      if('tags' in item.data ) {
         tags = item.data.tags;
       }
     })
-    return tags.filter(item => !tagsToRemove.includes(item))
+    return tags.filter(item => item !== 'blog'); // FIXME: !valuesToRemove.includes(item) non funziona con multipli valori!
   });
 };
