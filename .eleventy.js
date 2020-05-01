c9cb7bad1234ce42926bc44a25194477428f0503
Giulia Chiola <giulia.chiola.17@gmail.com>
2020-05-01 21:01:02 +0200

    feat: #13, add theme color pass through copy

--- .eleventy.js
+++ .eleventy.js
@@ -29,6 +29,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("js");
   eleventyConfig.addPassthroughCopy("google*.html"); // TODO: check this!
+  eleventyConfig.addPassthroughCopy("site-*.webmanifest");
   eleventyConfig.addPassthroughCopy("super-styleguide/src/css/style.css");
   eleventyConfig.setDataDeepMerge(true); // used to merge 'blog.11tydata.js' tags with .md tags
 
