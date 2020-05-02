270ce587779929fdd17c9bec478e5d13e2703088
Giulia Chiola <giulia.chiola.17@gmail.com>
2020-05-02 21:27:18 +0200

    feat: remove commented JS

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,6 @@
 const eleventyNavigation = require("@11ty/eleventy-navigation");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
-// function getRandomInt(min, max) {
-//   min = Math.ceil(min);
-//   max = Math.floor(max);
-//   return Math.floor(Math.random() * (max - min + 1)) + min;
-// }
-
-// function getRandomItem(arr) {
-//   return arr[getRandomInt(0, arr.length - 1)]
-// }
-
 function uniqueArray(arr) {
   return [...new Set(arr)]
 }
